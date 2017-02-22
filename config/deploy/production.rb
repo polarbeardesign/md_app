e0aaddf094fb6d8da1ddaaff9e39f644c31284e8
Jim Ivanoff <jim@polarbeardesign.net>
2017-02-22 11:23:03 -0600

    updated deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 load 'deploy/assets'
 
 set :user, 'marshall'
-set :domain, 'marshallingdetachemnt.org'
+set :domain, 'marshallingdetachment.org'
 
 set :application, "caf_md_app"
 set :repository,  "git@github.com:polarbeardesign/md_app.git"
@@ -28,8 +28,8 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
 # If you are using Passenger mod_rails uncomment this:
 
 # causes bundle install to run
- require "bundler/capistrano"
- set :bundle_flags, "--quiet --no-cache"
+# require "bundler/capistrano"
+# set :bundle_flags, "--quiet --no-cache"
 
 before "deploy:assets:precompile", "deploy:symlink_db_file"
 
